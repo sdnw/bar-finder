4708e0ee40a163a7b37d31e9f0cf5c5df49693b7
Steven <stevendawn98@gmail.com>
2022-11-15 20:50:57 -0500

    made links clickable

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 function Bar({ bar }) {
     const { name, brewery_type, street, city, state, postal_code, website_url } = bar;
+
+
     
     return (
         <li className="cards__item">
@@ -11,7 +13,9 @@ function Bar({ bar }) {
                     <p className="card__text">{brewery_type}</p>
                     <p className="card__text">{street}</p>
                     <p className="card__text">{city}, {state}, {postal_code}</p>
-                    <p className="card__text">{website_url}</p>
+                    <p className="card__text">
+                    {website_url ? <a href={website_url} target="_blank" rel="noreferrer">{website_url}</a> : null}
+                    </p>
             </div>
         </div>
         </li>
